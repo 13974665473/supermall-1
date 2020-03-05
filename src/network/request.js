c5d5b76cf18caf7b7c061fef98f75d0157237404
Juan2Tu <912300601@qq.com>
2020-03-05 15:41:25 +0800

    轮播图

--- src/network/request.js
+++ src/network/request.js
@@ -12,13 +12,11 @@ export function request(config) {
     console.log(err);
   })
 
-  // 2.2.响应拦截
   instance.interceptors.response.use(res => {
     return res.data
   }, err => {
     console.log(err);
   })
 
-  // 3.发送真正的网络请求
   return instance(config)
 }
