20bc223e85625f3a14b7f34777324e1a1c46f98b
Juan2Tu <912300601@qq.com>
2020-03-06 21:08:13 +0800

    首页商品数据展示

--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -5,6 +5,7 @@
     <recommend-view :recommends = 'recommends'></recommend-view>
     <feature-view></feature-view>
     <tab-control :titles="['流行','新款','精选']" class="tab-control"></tab-control>
+    <goods-list :goods = "goods['pop'].list"></goods-list>
     <ul>
       <li>列表</li>
       <li>列表</li>
@@ -118,17 +119,21 @@
 
   import NavBar from 'components/common/navbar/NavBar'
   import TabControl from 'components/content/tabControl/TabControl'
+  import GoodsList from 'components/content/goods/GoodsList'
 
-  import {getHomeMultidata} from 'network/home'
+  import {getHomeMultidata,
+          getHomeGoods} from 'network/home'
 
   export default {
     name: "Home",
     components: {
-      NavBar,
-      TabControl,
       HomeSwiper,
       RecommendView,
-      FeatureView
+      FeatureView,
+      NavBar,
+      TabControl,
+      GoodsList
+
     },
     data() {
       return {
@@ -136,18 +141,35 @@
         recommends: [],
         goods: {
           'pop': {page: 0,list: []},
-          'news': {page: 0,list: []},
+          'new': {page: 0,list: []},
           'sell': {page: 0,list: []},
         }
       }
     },
     created() {
       // 1.请求首页的多个数据
-      getHomeMultidata().then(res => {
-        this.banners = res.data.banner.list;
-        this.recommends = res.data.recommend.list;
-        
-      })
+      this.getHomeMultidata()
+      // 请求商品数据
+      this.getHomeGoods('pop')
+      this.getHomeGoods('new')
+      this.getHomeGoods('sell')
+      console.log(this.goods['pop'].list);
+      
+    },
+    methods: {
+      getHomeMultidata() {
+        getHomeMultidata().then(res => {
+          this.banners = res.data.banner.list;
+          this.recommends = res.data.recommend.list;
+        })
+      },
+      getHomeGoods(type) {
+        const page = this.goods[type].page + 1 
+        getHomeGoods(type, page).then(res => {
+          this.goods[type].list.push(...res.data.list)
+          this.goods[type].page += 1 
+        })
+      }
     }
   }
 </script>
