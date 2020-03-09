7d57d1e7a47c33efdbf6b3308a2ad9dbea843c53
Juan2Tu <912300601@qq.com>
2020-03-09 19:51:05 +0800

    封装了一个防抖函数

--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -31,6 +31,7 @@
   import BackTop from 'components/content/backTop/BackTop'
 
   import {getHomeMultidata,getHomeGoods} from 'network/home'
+  import {debounce} from 'common/utils'
 
   export default {
     name: "Home",
@@ -71,7 +72,7 @@
       this.getHomeGoods('sell')
     },
     mounted() {
-      const refresh = this.debounce(this.$refs.scroll.refresh, 100)
+      const refresh = debounce(this.$refs.scroll.refresh, 100)
         // 监听事件总线发送来的事件
         this.$bus.$on('itemImageLoad', () => {
         // 调用scroll的refresh刷新可滚动高度
@@ -82,16 +83,6 @@
     },
     methods: {
       /********* 事件监听********/
-      // 防抖函数
-      debounce(func, delay) {
-        let timer = null
-        return function (...args) {
-          if (timer) clearTimeout(timer)
-          timer = setTimeout(() => {
-            func.apply(this, args)
-          },delay)
-        }
-      },
       tabClick(index) {
         switch(index){
           case 0:
