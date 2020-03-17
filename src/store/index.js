7ab7a225f46bc9f069e2908af7204240e911e925
Juan2Tu <912300601@qq.com>
2020-03-17 15:54:31 +0800

    购物车完成

--- src/store/index.js
+++ src/store/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import Vuex from 'vuex'
 import mutations from './mutations'
 import actions from './actions'
+import getters from './getters'
 // 安装插件
 Vue.use(Vuex)
 
@@ -14,6 +15,7 @@ const store = new Vuex.Store({
   state,
   mutations,
   actions,
+  getters
 })
 
 // 挂载
